f6b67d1125b843878615855d48c73de75c3d9d2e
Hema Venkata <hvenkata@live.com>
2019-06-11 15:43:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,6 @@ function basicTeenager(age) {
   if(age > 12 && age < 20)
       return "You are a teenager!"
 }
-
 function teenager(age) {
   if(age > 12 && age < 20)
       return "You are a teenager!"
